9ca071a65cdef8eb67ee614192140717b3575034
Jakob Helgesson <jakob.helgesson@gmail.com>
2022-05-30 13:54:02 +0200

    Update Navbar (#15)

--- src/components/Navbar/NavLink.tsx
+++ src/components/Navbar/NavLink.tsx
@@ -10,7 +10,9 @@ export const NavbarLink: FC<{ name: string; path: string }> = ({
             to={path}
             className={({ isActive }) =>
                 `px-8 ${
-                    isActive ? 'text-neutral-100 border-b-4' : 'text-neutral-600'
+                    isActive
+                        ? 'text-neutral-100 border-b-4'
+                        : 'text-neutral-600'
                 }`
             }
         >
