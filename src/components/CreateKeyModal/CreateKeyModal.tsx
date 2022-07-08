5c3174a8a3fe80a13c51e19c9aa294268ca6ba03
Svemat01 <jakob.helgesson@gmail.com>
2022-07-09 00:38:23 +0200

    Add key permissions with permissio

--- src/components/CreateKeyModal/CreateKeyModal.tsx
+++ src/components/CreateKeyModal/CreateKeyModal.tsx
@@ -1,24 +1,43 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import { Button } from '@components/Button';
+import { Checkbox } from '@components/Checkbox';
 import { Modal } from '@components/Modal';
 import { cx } from '@utils/cx';
 import { environment } from '@utils/enviroment';
+import { KeyPerms } from '@utils/permissions';
 import { useJWT } from '@utils/useAuth';
 import { decode } from 'jsonwebtoken';
 import ms from 'ms';
-import { FC, useCallback, useState } from 'react';
+import { EMPTY_PERMISSIONS, grantPermission } from 'permissio';
+import { FC, useCallback, useMemo, useState } from 'react';
 import { Clipboard } from 'react-feather';
 import { FieldErrors, useForm } from 'react-hook-form';
+import { useDebounce } from 'use-debounce';
 import { useSignMessage } from 'wagmi';
 
-type CreateKeyForm = {
-    permissions: string;
-    name: string;
-    expires: boolean;
-    expiresIn: string;
+const Perms = {
+    'Read Applications': KeyPerms.APPS_READ,
+    'Modify/Create Applications': KeyPerms.APPS_WRITE,
+    'Delete Applications': KeyPerms.APPS_DELETE,
+
+    'Read Domains': KeyPerms.DOMAINS_READ,
+    'Modify/Create Domains': KeyPerms.DOMAINS_WRITE,
+    'Delete Domains': KeyPerms.DOMAINS_DELETE,
+
+    'Read Deployments': KeyPerms.DEPLOYMENTS_READ,
+    'Modify/Create Deployments': KeyPerms.DEPLOYMENTS_WRITE,
+    'Delete Deployments': KeyPerms.DEPLOYMENTS_DELETE,
 };
 
+type CreateKeyForm = {
+    name: string;
+    permanent: boolean;
+    expiresIn: string;
+    'Full Access': boolean;
+} & Record<keyof typeof Perms, boolean>;
+
 export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
+    const [noPerms, setNoPerms] = useState(false);
     const {
         register,
         handleSubmit,
@@ -29,26 +48,37 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
         delayError: 100,
         mode: 'all',
         resolver: async (values) => {
-            const errors: FieldErrors<typeof values> = {};
+            const ValueErrors: FieldErrors<typeof values> = {};
 
-            if (values.name.length === 0) errors.name = { type: 'minLength' };
+            if (values.name.length === 0)
+                ValueErrors.name = { type: 'minLength' };
 
-            if (values.permissions.length === 0)
-                errors.permissions = { type: 'minLength' };
-
-            if (values.expires) {
-                try {
-                    ms(values.expiresIn);
-                } catch {
-                    errors.expiresIn = {
-                        type: 'pattern',
-                        message: 'Not a valid timestamp',
-                    };
-                }
+            if (
+                !values.permanent &&
+                values.expiresIn &&
+                !ms(values.expiresIn)
+            ) {
+                ValueErrors.expiresIn = {
+                    type: 'pattern',
+                    message: 'Not a valid timestamp',
+                };
             }
 
-            if (Object.keys(errors).length > 0) {
-                return { values: {}, errors };
+            if (
+                Object.keys(Perms).every(
+                    (key) => !values[key as keyof typeof Perms]
+                ) &&
+                !values['Full Access']
+            ) {
+                // errors needs to be a part of the form, otherwise it will be ignored
+                // Thus we create a state for when no perms have been selected.
+                if (!noPerms) setNoPerms(true);
+            } else {
+                if (noPerms) setNoPerms(false);
+            }
+
+            if (Object.keys(ValueErrors).length > 0) {
+                return { values: {}, errors: ValueErrors };
             }
 
             return {
@@ -57,8 +87,8 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
             };
         },
         defaultValues: {
-            expires: true,
-            permissions: '-1',
+            permanent: false,
+            name: '',
             expiresIn: '10h',
         },
     });
@@ -74,18 +104,26 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
             owner_id: string;
         };
 
+        const rawPerms = data['Full Access']
+            ? [KeyPerms.FULL]
+            : Object.keys(Perms)
+                  .filter((key) => data[key as keyof typeof Perms])
+                  .map((key) => Perms[key as keyof typeof Perms]);
+
+        const permissions = grantPermission(EMPTY_PERMISSIONS, ...rawPerms);
+
         console.log({ decodedToken });
         const formData: {
             permissions: string;
             name: string;
             expiresIn?: string;
         } = {
-            permissions: data.permissions,
+            permissions: permissions.toString(),
             name: data.name,
-            expiresIn: (data.expires && data.expiresIn) || '',
+            expiresIn: (data.permanent && data.expiresIn) || '',
         };
 
-        if (!data.expires) delete formData['expiresIn'];
+        if (!data.permanent) delete formData['expiresIn'];
 
         const payload = {
             action: 'CREATE_KEY',
@@ -117,6 +155,17 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
         setGeneratedToken((await domain_response.json())['token']);
     }, []);
 
+    const [expireDebounce] = useDebounce(watch('expiresIn'), 300);
+    const expireString = useMemo(() => {
+        if (expireDebounce === undefined) return '';
+
+        try {
+            return ms(ms(expireDebounce), { long: true });
+        } catch {
+            return '';
+        }
+    }, [expireDebounce]);
+
     return (
         <Modal label="Key Creator" onClose={onClose}>
             {!generatedToken && (
@@ -145,45 +194,46 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
                         </div>
                     </div>
                     <div>
-                        <label
-                            htmlFor="permissions"
-                            className="block text-sm font-medium text-neutral-900 dark:text-neutral-300 mb-2"
-                        >
-                            Raw Permissions
-                        </label>
-                        <div className="flex items-center gap-2 text-neutral-500">
-                            <input
-                                type="text"
-                                id="permissions"
-                                className={cx(
-                                    'text-sm rounded-lg block w-full p-2.5 border',
-                                    errors.permissions
-                                        ? 'bg-red-900 bg-opacity-20 border-red-500 focus-visible:outine-red-500'
-                                        : 'focus:ring-blue-500 focus:border-blue-500 bg-neutral-50 border-neutral-300 dark:bg-neutral-600 dark:border-neutral-500 dark:placeholder-neutral-400 dark:text-white'
-                                )}
-                                placeholder="32,11"
-                                required
-                                {...register('permissions')}
+                        <span className="block text-base font-medium text-neutral-900 dark:text-neutral-300 mb-2">
+                            Key Permissions
+                        </span>
+                        <div className="grid items-center gap-2 text-neutral-500">
+                            <Checkbox
+                                id="Full Access"
+                                label="Full Access"
+                                register={register}
                             />
+                            {Object.keys(Perms).map((perm) => (
+                                <Checkbox
+                                    id={perm}
+                                    key={perm}
+                                    label={perm}
+                                    register={register}
+                                    disabled={watch('Full Access')}
+                                />
+                            ))}
+
+                            {noPerms && (
+                                <span className="text-red-500">
+                                    No permissions selected.
+                                </span>
+                            )}
                         </div>
                     </div>
                     <div>
-                        <label
-                            htmlFor="expires"
-                            className="block text-sm font-medium text-neutral-900 dark:text-neutral-300 mb-2"
-                        >
-                            Expires
-                        </label>
+                        <span className="block text-base font-medium text-neutral-900 dark:text-neutral-300 mb-2">
+                            Settings
+                        </span>
                         <div className="block w-full">
-                            <input
-                                type="checkbox"
-                                id="expires"
-                                className="text-sm rounded-lg block p-2.5 border focus:ring-blue-500 focus:border-blue-500 bg-neutral-50 border-neutral-300 dark:bg-neutral-600 dark:border-neutral-500 dark:placeholder-neutral-400 dark:text-white"
-                                {...register('expires')}
+                            <Checkbox
+                                id="permanent"
+                                label="Permanent Key"
+                                // className="text-sm rounded-lg block p-2.5 border focus:ring-blue-500 focus:border-blue-500 bg-neutral-50 border-neutral-300 dark:bg-neutral-600 dark:border-neutral-500 dark:placeholder-neutral-400 dark:text-white"
+                                register={register}
                             />
                         </div>
                     </div>
-                    {watch('expires') && (
+                    {!watch('permanent') && (
                         <div>
                             <label htmlFor="expiresIn">Expires In</label>
 
@@ -202,16 +252,22 @@ export const CreateKeyModal: FC<{ onClose: () => void }> = ({ onClose }) => {
                                     {...register('expiresIn')}
                                 />
                             </div>
+
+                            {expireString && (
+                                <div className="mt-2 text-neutral-500">
+                                    {expireString}
+                                </div>
+                            )}
                         </div>
                     )}
                     <Button
                         type="submit"
-                        disabled={isSubmitting || !isValid}
+                        disabled={isSubmitting || !isValid || noPerms}
                         pending={isSubmitting}
                         variant="primary"
-                        className="w-full whitespace-pre justify-center"
+                        className="mt-4 w-full whitespace-pre justify-center"
                         label={
-                            isValid
+                            isValid && !noPerms
                                 ? isSubmitting
                                     ? 'Pending...'
                                     : 'Create 🔑'
